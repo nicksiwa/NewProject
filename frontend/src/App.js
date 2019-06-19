c962abd1515b3f17f7858d2045170ca5ca936990
Siwa Khongsuphap <siwa@hospital-os.com>
2019-06-19 14:07:17 +0700

    test webhook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Input, Button } from 'antd';
 
 function App() {
   return (
     <div className="app">
-      <Input name="test" />
-      <Button>Click</Button>
+      Test Webhook
     </div>
   );
 }
